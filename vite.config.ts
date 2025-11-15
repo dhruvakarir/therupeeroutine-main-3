f701cd3cde1dc2447428c92b315777215a278579
Dhruv <dhruv@Dhruvs-MacBook-Air.local>
2025-11-15 18:37:57 +0530

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/therupeeroutine-main-3/", // Replace <repository-name> with your GitHub repo name
+  base: "/", // Replace <repository-name> with your GitHub repo name
   server: {
     host: "::",
     port: 8080,
